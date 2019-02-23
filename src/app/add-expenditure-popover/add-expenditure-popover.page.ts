d0ab80f9c0d1430b74bd10f85fc1b01118c6ef3b
Youran Qin <u5255157@anu.edu.au>
2019-02-23 22:57:16 +0800

    add expenditure

--- src/app/add-expenditure-popover/add-expenditure-popover.page.ts
+++ src/app/add-expenditure-popover/add-expenditure-popover.page.ts
@@ -1,17 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Expenditure {
+  expenditureNumber: number
+  expenditureType: string
+}
+
 @Component({
   selector: 'app-add-expenditure-popover',
   templateUrl: './add-expenditure-popover.page.html',
   styleUrls: ['./add-expenditure-popover.page.scss'],
 })
+
 export class AddExpenditurePopoverPage implements OnInit {
-  expenditure: number = 2.00;
+  newExpenditure: Expenditure = {
+    expenditureNumber: 100,
+    expenditureType: '餐饮'
+  }
+
+  expenditureTypeList: string[] = ['餐饮', '购物', '社交', '房租'];
+  expenditureList: Expenditure[] = [];
 
   constructor() { }
 
   ngOnInit() {
 
+  }
+
+  addExpenditure () {
+    this.expenditureList.push(this.newExpenditure);
   }
 
 }
